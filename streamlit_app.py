56fd9c28932d1bd856110d399f6eec4485adc1cd
Nur Chamid <39608830+nurchamid@users.noreply.github.com>
2021-11-17 16:32:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import plotly.express as px
 import nltk
 import plotly.figure_factory as ff
