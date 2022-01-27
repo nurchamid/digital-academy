f7c6c250e55ab531e16c56299135134eb37bb661
Nur Chamid <39608830+nurchamid@users.noreply.github.com>
2022-01-27 14:10:33 +0700

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -6,7 +6,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from io import StringIO
-from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score
 import matplotlib.pyplot as plt
